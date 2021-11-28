0ad690aa38541effc5ebfcf3fbb8b4254e487687
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 00:09:38 +0530

    Added : Create a Leap Year Program by scanner

--- src/com/bridgelabz/LeapYear.java
+++ src/com/bridgelabz/LeapYear.java
@@ -5,6 +5,8 @@ public class LeapYear {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to java Program");
+
         int year;
         System.out.println("Enter an Year : ");
         Scanner sc = new Scanner(System.in);
