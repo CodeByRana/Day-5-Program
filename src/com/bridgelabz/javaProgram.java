fa48b95c2f3ba0f84a6f4fd9fdd2bd4927a3882e
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 00:33:42 +0530

    Added : Create a Harmonic Number series

--- src/com/bridgelabz/javaProgram.java
+++ src/com/bridgelabz/javaProgram.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 
 public class javaProgram {
 
+    public static do25uble Harmonic(int n){
+        double sum =0;
+        if(n>0){
+            for(double i=1;i<=n;i++){
+                sum+=(1/i);
+            }
+        }else{
+            System.out.println("INVALID USER INPUT");
+        }
+        return sum;
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to java Program");
-
-        int b, e;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the base: ");
-        b = sc.nextInt();
-        System.out.print("Enter the exponent: ");
-        e = sc.nextInt();
-        //invokes the pow() method of the Math class
-        double power = Math.pow(b, e);
-        //prints result
-        System.out.println(b +" to the power " +e + " is: "+power);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        System.out.println(Harmonic(n));
+        sc.close();
     }
 }
