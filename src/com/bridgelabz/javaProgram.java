17e7503fc67a59f034ed9659d03dbfe7f98db0ba
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 00:56:32 +0530

    Added : create a swap two number

--- src/com/bridgelabz/javaProgram.java
+++ src/com/bridgelabz/javaProgram.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 public class javaProgram {
 
     public static void main(String[] args) {
-
-        System.out.println("Enter the value of num1 and num2");
+        System.out.println("Enter the value of x and y");
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int quotient = 0, remainder = 0;
-
-
-        quotient = num1 / num2;
-
-        remainder = num1 % num2;
-
-        System.out.println("Quotient when " + num1 + "/" + num2 + " is: " + quotient);
-        System.out.println("Remainder when " + num1 + " is divided by " + num2 + " is: " + remainder);
+        /*Define variables*/
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("before swapping numbers: "+x +" "+ y);
+        /*Swapping*/
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("After swapping: "+x +"  " + y);
     }
 }
