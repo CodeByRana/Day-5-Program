2d4cd4d20a4f4e090edc8fa875dd3ab086285139
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 01:09:24 +0530

    Added: Create a program LargestAmongThreeNumber

--- src/com/bridgelabz/javaProgram.java
+++ src/com/bridgelabz/javaProgram.java
@@ -5,13 +5,21 @@ public class javaProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter a character :");
+        int a, b, c, largest, temp;
+        //object of the Scanner class
         Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(0);
-        if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
-            System.out.println("Given character is an vowel");
-        }else{
-            System.out.println("Given character is a consonant");
-        }
+        //reading input from the user
+        System.out.println("Enter the first number:");
+        a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        c = sc.nextInt();
+        //comparing a and b and storing the largest number in a temp variable
+        temp=a>b?a:b;
+        //comparing the temp variable with c and storing the result in the variable
+        largest=c>temp?c:temp;
+        //prints the largest number
+        System.out.println("The largest number is: "+largest);
     }
 }
