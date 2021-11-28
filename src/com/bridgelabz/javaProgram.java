37e958f3ef3e8b8b8269a27ece8482a6bafa0539
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 00:37:28 +0530

    Added : Create a Flip Coin Program

--- src/com/bridgelabz/javaProgram.java
+++ src/com/bridgelabz/javaProgram.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 
 public class javaProgram {
 
-    public static do25uble Harmonic(int n){
-        double sum =0;
-        if(n>0){
-            for(double i=1;i<=n;i++){
-                sum+=(1/i);
-            }
-        }else{
-            System.out.println("INVALID USER INPUT");
-        }
-        return sum;
-
-    }
-
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n");
-        int n = sc.nextInt();
-        System.out.println(Harmonic(n));
-        sc.close();
+            Scanner sc = new Scanner(System.in);
+            
+            System.out.println("Enter the number of times FLIP the COIN*");
+            int n = sc.nextInt();
+            System.out.println("Enter the number of times FLIP the COIN*");
+            int head =0,tail=0;
+            for(int i=0;i<n;i++){
+                if(Math.random()<0.5){
+                    head++;
+                }else{
+                    tail++;
+                }
+            }
+            int hper = (head*100)/n;
+            int tper = (tail*100)/n;
+            System.out.println("Head Percentage==>>"+hper+"%");
+            System.out.println("Tail Percentage==>>"+tper+"%");
+            System.out.println("HEADS ==>> "+head);
+            System.out.println("TAILS ==>> "+tail);
     }
 }
