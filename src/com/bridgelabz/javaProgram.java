fb4f7eb36d8fcc92982acf04eddb98a7a7bb7819
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 01:05:22 +0530

    Added: Create a program VowelOrConsonant

--- src/com/bridgelabz/javaProgram.java
+++ src/com/bridgelabz/javaProgram.java
@@ -5,17 +5,13 @@ public class javaProgram {
 
     public static void main(String[] args) {
 
-        int n;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
-        n = s.nextInt();
-        if(n % 2 == 0)
-        {
-            System.out.println("The given number "+n+" is Even ");
-        }
-        else
-        {
-            System.out.println("The given number "+n+" is Odd ");
+        System.out.println("Enter a character :");
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+            System.out.println("Given character is an vowel");
+        }else{
+            System.out.println("Given character is a consonant");
         }
     }
 }
